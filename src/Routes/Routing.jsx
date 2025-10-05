d37b71c6f5d6225d1a85020a55b71266cec3bec9
29Jeanca <jeancarlosbarberena29@gmail.com>
2025-10-04 22:36:21 -0600

    first commit

---
+++ src/Routes/Routing.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+
+const Routing = () =>{
+    return(
+        <Router>
+            <Routes>
+                <Route/>
+            </Routes>
+        </Router>
+    )
+}
+export default Routing;
